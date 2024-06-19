27a8bb1fdfff56ceb2650683065ec7f5a9e21d6f
PavanKumarN0809 <168434437+Pavanpk8090@users.noreply.github.com>
2024-06-19 18:54:55 +0530

    Update Tunes.py

--- conditions/Stark.py
+++ conditions/Stark.py
@@ -1,9 +1,29 @@
 num1=input("Enter the number:")
 num2=input("Enter the number:")
 num3=input("Enter the number:")
-if num1 is num2 is num3:
+if num1==num2==num3:
     print("Equilateral triangle")
-elif num1 is num2 or num2 is num3 or num1 is num3:
+elif num1==num2 or num1==num3 or num3==num3:
     print("Isoceleus triangle")
 else:
     print("Scalene triangle")
+
+user1=input("Enter the name:")
+user2=input("Enter the name:")
+if user1==user2:
+    print("Draw")
+elif user1=="Rock" and user2=="Paper":
+    print("Paper won")
+elif user1=="Paper" and user2=="Scissor":
+    print("Scissor won")
+elif user1=="Scissor" and user2=="Rock":
+    print("Rock won")
+elif user1=="Paper" and user2=="Rock":
+    print("Paper won")
+elif user1=="Scissor" and user2=="Paper":
+    print("Scissor won")
+elif user1=="Rock" and user2=="Scissor":
+    print("Rock won")
+else:
+    print("Invalid")
+
